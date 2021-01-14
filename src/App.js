4f5595e1c93ae36c31ccabe1525b4fa85b58574f
DanaDovzh <danadovzh16@gmail.com>
2021-01-14 17:50:09 +0200

    Fix App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 import './css/style.css';
-import Charges from './components/Charges';
 import { Setting } from './components/Settings/Setting';
 import { useEffect } from 'react';
-=======
-import logo from "./logo.svg";
 import "./App.css";
 import Charges from "./components/Table/Charges";
 import Incomes from "./components/Table/Incomes";
-import { useEffect } from "react";
->>>>>>> ae8a6134a3df54b7568d4ac6b3cb1bd4f774088e
 
 function App() {
   useEffect(() => {
@@ -18,11 +12,8 @@ function App() {
   return (
     <div className="App">
       <Charges />
-<<<<<<< HEAD
       <Setting />
-=======
       <Incomes />
->>>>>>> ae8a6134a3df54b7568d4ac6b3cb1bd4f774088e
     </div>
   );
 }
