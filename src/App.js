5a84bec2339cae013a6a68547670c238ef998411
maryna.babkina <babckina@gmail.com>
2021-01-17 13:16:36 +0200

    App edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import './css/style.css'
-import { Setting } from './components/Settings/Setting'
-import { useEffect } from 'react'
-import './App.css'
-import Charges from './components/Table/Charges'
-import Incomes from './components/Table/Incomes'
-import Graphs from './components/Graphs/Graphs'
-
+import "./css/style.css";
+import { Setting } from "./components/Settings/Setting";
+import { useEffect } from "react";
+import "./App.css";
+import Charges from "./components/Table/Charges";
+import Incomes from "./components/Table/Incomes";
+import Graphs from "./components/Graphs/Graphs";
 function App() {
   useEffect(() => {
-    localStorage.setItem('charges', JSON.stringify([]))
-  })
+    localStorage.setItem("charges", JSON.stringify([]));
+  });
   return (
-    <div className='App'>
+    <div className="App">
       <Charges />
       <Incomes />
       <Setting />
       <Graphs />
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
