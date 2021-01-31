ab67b7b8a283e9f54c20959ace0a5c52d0f7e119
sasha9756475 <sasha13891@gmail.com>
2021-01-31 12:38:45 +0200

    added warning by limit in percents

--- src/components/Settings/Reminder/Reminder.js
+++ src/components/Settings/Reminder/Reminder.js
@@ -1,83 +1,114 @@
 import React, { useState } from "react";
 import { AlertOwn } from "../PartsForm/Alert";
-import { Checkbox } from '@material-ui/core';
-import { Button } from '@material-ui/core';
-import { Header } from '../PartsForm/Header';
+import { Checkbox } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import { Header } from "../PartsForm/Header";
 import { Input } from "../PartsForm/Input";
 
-import { parseData, setItemsToLocalStorage } from './untils'
+import { parseData, setItemsToLocalStorage } from "./untils";
 
-import './reminder.sass';
+import "./reminder.sass";
 
 export const Reminder = () => {
-    const data = parseData('limit')
-    const [isReminderChecked, setIsReminderChecked] = useState(data !== null ? data.remind : false)
-    const [limit, setLimit] = useState(data !== null ? data.limit : '')
+  const data = parseData("limit");
+  const [isReminderChecked, setIsReminderChecked] = useState(
+    data !== null ? data.remind : false
+  );
+  const [limit, setLimit] = useState(data !== null ? data.limit : "");
+  const [limitInPercents, setLimitInPercents] = useState(
+    data !== null ? data.limitInPercents : ""
+  );
 
-    const [open, setOpen] = useState(true);
-    const [alert, setAlert] = useState(false);
+  const [open, setOpen] = useState(true);
+  const [alert, setAlert] = useState(false);
 
-    const resetAlert = () => {
-        setOpen(false);
-        setAlert(false);
-    };
+  const resetAlert = () => {
+    setOpen(false);
+    setAlert(false);
+  };
 
-    const sendData = () => {
-        let reminder = {};
-        if (isReminderChecked && +limit > 0) {
-            reminder = {
-                limit: limit,
-                remind: true
-            }
-        } else if (!isReminderChecked) {
-            reminder = {
-                limit: '',
-                remind: false
-            }
-        }
-        setAlert(true);
-        setOpen(true);
-        setTimeout(resetAlert, 2000);
-        setItemsToLocalStorage('limit', reminder);
+  const createObject = () => {
+    let reminder = {};
+    if (
+      isReminderChecked &&
+      +limit >= 0 &&
+      limitInPercents >= 0 &&
+      limitInPercents <= 100
+    ) {
+      reminder = {
+        limit: limit,
+        limitInPercents: limitInPercents,
+        remind: true,
+      };
+    } else if (!isReminderChecked) {
+      reminder = {
+        limit: "",
+        limitInPercents: "",
+        remind: false,
+      };
     }
 
-    return (
-        <>
-            <div className='wrapper'>
-                <Header type='Reminder' />
-                <div className='reminder-settings'>
-                    <Input
-                        type='number'
-                        value={limit}
-                        onChange={e => setLimit(e.target.value)}
-                        disabled={!isReminderChecked}
-                        placeholder='Your limit'
-                        label='Set the value of limit'
-                    />
-                    <div className='d-flex'>
-                        <Checkbox
-                            checked={isReminderChecked}
-                            onChange={() => setIsReminderChecked(!isReminderChecked)}
-                        />
-                        <label className='remind-check'>remind me when my balance will be lower than limit</label>
-                    </div>
-                    {+limit <= 0 && limit ? <p>Нou have entered a number that is less than or equal to 0</p> : null}
+    return reminder;
+  };
 
-                    <Button
-                        variant="contained"
-                        fullWidth
-                        onClick={sendData}>
-                        save changes
-                    </Button>
-                </div>
-            </div>
-            {alert &&
-                <AlertOwn
-                    open={open}
-                    resetAlert={resetAlert}
-                    text='Limit saved'
-                />
-            }
-        </>
-    )
-}
+  const sendData = () => {
+    const reminder = createObject();
+    setAlert(true);
+    setOpen(true);
+    setTimeout(resetAlert, 2000);
+    setItemsToLocalStorage("limit", reminder);
+  };
+
+  return (
+    <>
+      <div className="wrapper">
+        <Header type="Reminder" />
+        <div className="reminder-settings">
+          <Input
+            type="number"
+            value={limit}
+            onChange={(e) => setLimit(e.target.value)}
+            disabled={!isReminderChecked}
+            placeholder="Your limit"
+            label="Set the value of limit"
+          />
+
+          <Input
+            type="number"
+            value={limitInPercents}
+            onChange={(e) => setLimitInPercents(e.target.value)}
+            disabled={!isReminderChecked}
+            placeholder="Your percentage limit"
+            label="Set the value of a percentage limit"
+          />
+
+          <div className="d-flex">
+            <Checkbox
+              checked={isReminderChecked}
+              onChange={() => setIsReminderChecked(!isReminderChecked)}
+            />
+            <label className="remind-check">
+              remind me when my balance will be lower than limit
+            </label>
+          </div>
+
+          {+limit <= 0 && limit ? (
+            <p className="m-botton">
+              Нou have entered a number that is less than or equal to 0
+            </p>
+          ) : null}
+          {+limitInPercents < 0 || limitInPercents > 100 ? (
+            <p className="m-botton">You entered uncorrec percents value</p>
+          ) : null}
+
+          <Button variant="contained" fullWidth onClick={sendData}>
+            save changes
+          </Button>
+        </div>
+      </div>
+      {alert && (
+        <AlertOwn open={open} resetAlert={resetAlert} text="Limit saved" />
+      )}
+    </>
+  );
+};
--- src/components/Table/AddElementModal.js
+++ src/components/Table/AddElementModal.js
@@ -1,33 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import PostAddIcon from '@material-ui/icons/PostAdd';
-import TablePortal from './TablePortal';
-import { Input } from './PartForTable/Input';
-import { ButtonsForTable } from './PartForTable/ButtonsForTable';
-import { parseData } from '../Settings/Reminder/untils';
-import './AddElementModal.sass';
+import React, { useEffect, useState } from "react";
+import PostAddIcon from "@material-ui/icons/PostAdd";
+import TablePortal from "./TablePortal";
+import { Input } from "./PartForTable/Input";
+import { ButtonsForTable } from "./PartForTable/ButtonsForTable";
+import { parseData } from "../Settings/Reminder/untils";
+import "./AddElementModal.sass";
 
 const AddElementModal = ({ title, isModalOpen, setModalOpen, setElements }) => {
-  const [enteredSum, setEnteredSum] = useState('');
+  const [enteredSum, setEnteredSum] = useState("");
   const category = React.createRef();
   const description = React.createRef();
   const money = React.createRef();
   let elementsArr = [];
-  let limitData = parseData('limit');
+  let limitData = parseData("limit");
 
-  const  isItChargeTable=title==='charge'?true:false;
+  const isItChargeTable = title === "charge" ? true : false;
 
-  if (limitData === null) {
+  if (
+    limitData === null ||
+    (!limitData.limit && !limitData.remind && !limitData.limitInPercents)
+  ) {
     limitData = {
-      limit: '0',
+      limit: "0",
+      limitInPercents: "0",
       remind: false,
     };
+  } else {
+    if (limitData.limit === "") {
+      limitData = { ...limitData, limit: null };
+    }
+    if (limitData.limitInPercents === "") {
+      limitData = { ...limitData, limitInPercents: null };
+    }
   }
-  const { limit, remind } = limitData;
-
-  const balance = parseData('balance');
+  const { limit, remind, limitInPercents } = limitData;
+  const balance = parseData("balance");
 
   useEffect(() => {
-    elementsArr = JSON.parse(localStorage.getItem(title + 's'));
+    elementsArr = JSON.parse(localStorage.getItem(title + "s"));
   });
 
   function addElement(e) {
@@ -40,77 +50,90 @@ const AddElementModal = ({ title, isModalOpen, setModalOpen, setElements }) => {
         money: money.current.value,
         date: new Date().toUTCString(),
       });
-      description.current.value = '';
-      money.current.value = '';
+      description.current.value = "";
+      money.current.value = "";
     }
     console.log(elementsArr);
-    localStorage.setItem(title + 's', JSON.stringify(elementsArr));
-    setElements(JSON.parse(localStorage.getItem(title + 's')));
+    localStorage.setItem(title + "s", JSON.stringify(elementsArr));
+    setElements(JSON.parse(localStorage.getItem(title + "s")));
     setModalOpen(false);
   }
+
+
+
+  const showReminder = () => {
+    if (isItChargeTable && remind) {
+      if (limit) {
+        if (balance - enteredSum < limit) return true;
+      }
+      if (limitInPercents) {
+        const limit = (balance * limitInPercents) / 100;
+        if (balance - enteredSum < limit) return true;
+      }
+    }
+    return false;
+  };
 
   return (
     <>
       {isModalOpen && (
         <TablePortal>
-          <div className='modal-overlay'>
-            <div className='modal-window'>
-              <div className='modal-header'>
-                <PostAddIcon fontSize='small' />
+          <div className="modal-overlay">
+            <div className="modal-window">
+              <div className="modal-header">
+                <PostAddIcon fontSize="small" />
                 Add {title}
-                <select id='categories' ref={category}>
+                <select id="categories" ref={category}>
                   <option disabled>Pick category</option>
-                  {JSON.parse(localStorage.getItem(title + 'Categories')).map(
+                  {JSON.parse(localStorage.getItem(title + "Categories")).map(
                     (category) => (
                       <option value={category.name} key={category.categoryId}>
                         {category.name}
                       </option>
-                    ),
+                    )
                   )}
                 </select>
               </div>
-              <div className='modal-body'>
+              <div className="modal-body">
                 <Input
-                  key='9iu8o78kj9hj79kh87jkh'
+                  key="9iu8o78kj9hj79kh87jkh"
                   ref={description}
-                  id='description'
-                  type='text'
-                  label='Description'
-                  name='description'
+                  id="description"
+                  type="text"
+                  label="Description"
+                  name="description"
                 />
                 <Input
-                  key='21xvc5v1cv23b4ghg5'
+                  key="21xvc5v1cv23b4ghg5"
                   ref={money}
-                  id='money'
-                  type='number'
-                  label='Money'
-                  name='money'
+                  id="money"
+                  type="number"
+                  label="Money"
+                  name="money"
                   value={enteredSum}
                   onChange={(e) => setEnteredSum(e.target.value)}
                 />
               </div>
 
-              {limit !== null && remind && isItChargeTable ? (
-                +balance - +enteredSum < limit ? (
-                  <p>
-                    if you add this cost your balance will be lower than the
-                    limit
-                  </p>
-                ) : null
+              {showReminder() ? (
+                <p>
+                  if you add this cost your balance will be lower than the limit
+                </p>
               ) : null}
 
-              <div className='modal-footer'>
+
+              <div className="modal-footer">
                 <ButtonsForTable
-                  id='btn-add'
-                  className={'btn-add'}
-                  key='bn87mb87b6n765fg4'
+                  id="btn-add"
+                  className={"btn-add"}
+                  key="bn87mb87b6n765fg4"
                   clickBtn={() => setModalOpen(false)}
                 >
                   Close
                 </ButtonsForTable>
                 <ButtonsForTable
-                  key='f87gd98f7gd8fg'
-                  className={'btn-add'}
+                  key="f87gd98f7gd8fg"
+                  className={"btn-add"}
                   clickBtn={addElement}
                 >
                   Add
